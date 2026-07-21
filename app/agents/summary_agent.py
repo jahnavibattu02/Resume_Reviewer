aa0c496835ca225e6cd2e0517fbbf02aa8c644e4
Jahnavi Battu <86176181+jahnavibattu02@users.noreply.github.com>
2026-07-20 22:16:51 -0400

    Implement summary agent to generate resume summary

--- app/agents/summary_agent.py
+++ app/agents/summary_agent.py
@@ -1,1 +1,14 @@
 
+from app.graph.state import ResumeState
+from app.services.resume_service import resume_service
+
+
+def summary_agent(state: ResumeState):
+
+    summary = resume_service.generate_summary(
+        state["resume"]
+    )
+
+    state["summary"] = summary
+
+    return state
