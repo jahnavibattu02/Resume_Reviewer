7d9a19f36d049ddeb91da45382d0eee39d24611d
Jahnavi Battu <86176181+jahnavibattu02@users.noreply.github.com>
2026-07-20 22:17:38 -0400

    Add suggestion_agent function to generate suggestions

--- app/agents/suggestion_agent.py
+++ app/agents/suggestion_agent.py
@@ -1,1 +1,18 @@
 
+from app.graph.state import ResumeState
+from app.services.resume_service import resume_service
+
+
+def suggestion_agent(state: ResumeState):
+
+    suggestions = resume_service.generate_suggestions(
+
+        state["summary"],
+
+        state["target_role"]
+
+    )
+
+    state["suggestions"] = suggestions
+
+    return state
