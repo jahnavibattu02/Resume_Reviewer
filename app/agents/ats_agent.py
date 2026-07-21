1a93fc760b9e2a068b97e42fb0aa17c1cac58239
Jahnavi Battu <86176181+jahnavibattu02@users.noreply.github.com>
2026-07-20 22:17:23 -0400

    Add ATS agent function to calculate ATS score

--- app/agents/ats_agent.py
+++ app/agents/ats_agent.py
@@ -1,1 +1,16 @@
 
+from app.graph.state import ResumeState
+from app.services.resume_service import resume_service
+
+
+def ats_agent(state: ResumeState):
+
+    ats = resume_service.generate_ats_score(
+
+        state["summary"]
+
+    )
+
+    state["ats_score"] = ats
+
+    return state
