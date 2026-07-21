3ce4526e5e508c136448d6e1a13e3faa43d862c1
Jahnavi Battu <86176181+jahnavibattu02@users.noreply.github.com>
2026-07-20 22:18:15 -0400

    Implement workflow with state graph and agents

--- app/graph/workflow.py
+++ app/graph/workflow.py
@@ -1,1 +1,107 @@
 
+from langgraph.graph import StateGraph, END
+
+from app.graph.state import ResumeState
+
+from app.agents.summary_agent import summary_agent
+from app.agents.feedback_agent import feedback_agent
+from app.agents.ats_agent import ats_agent
+from app.agents.suggestion_agent import suggestion_agent
+from app.agents.report_agent import report_agent
+
+
+workflow = StateGraph(ResumeState)
+
+# Nodes
+
+workflow.add_node(
+
+    "summary",
+
+    summary_agent
+
+)
+
+workflow.add_node(
+
+    "feedback",
+
+    feedback_agent
+
+)
+
+workflow.add_node(
+
+    "ats",
+
+    ats_agent
+
+)
+
+workflow.add_node(
+
+    "suggestions",
+
+    suggestion_agent
+
+)
+
+workflow.add_node(
+
+    "report",
+
+    report_agent
+
+)
+
+# Entry
+
+workflow.set_entry_point(
+
+    "summary"
+
+)
+
+# Flow
+
+workflow.add_edge(
+
+    "summary",
+
+    "feedback"
+
+)
+
+workflow.add_edge(
+
+    "feedback",
+
+    "ats"
+
+)
+
+workflow.add_edge(
+
+    "ats",
+
+    "suggestions"
+
+)
+
+workflow.add_edge(
+
+    "suggestions",
+
+    "report"
+
+)
+
+workflow.add_edge(
+
+    "report",
+
+    END
+
+)
+
+graph = workflow.compile()
